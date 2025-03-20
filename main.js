78da8b1c6d93d070aef7aa522b4821214b992d6e
littlezabi <zohaibjozvi@gmail.com>
2025-03-20 21:01:34 +0500

    button added and update js code

--- main.js
+++ main.js
@@ -1,26 +1,34 @@
 const content = document.getElementById("content");
-const text = "BLUETERMINAL";
-const limit = text.length;
+const textList = ["SUBSCRIBE", "BLUETERMINAL", "IF", "YOU", "LIKE!", "Thanks :)"];
 const strings = "abcdefghijklmnopqrstuvwxyz_-+?$%^&*()!@#$%";
 
-for (let i = 0; i < limit; i++) {
-  const span = document.createElement("span");
-  content.append(span);
-}
+let index = 0;
 
-const elements = document.querySelectorAll("#content span");
+const showText = () => {
+    content.innerHTML = "";
+    const text = textList[index];
+    const limit = text.length;
 
-elements.forEach((span, index) => {
-  const targetChar = text[index];
+    for (let i = 0; i < limit; i++) {
+        const span = document.createElement("span");
+        content.append(span);
+    }
 
-  let randTime = Math.ceil(Math.random() * 100) + 50;
-  let interval = setInterval(() => {
-    let randIndex = Math.ceil(Math.random() * (strings.length - 1));
-    span.innerHTML = strings[randIndex];
-  }, randTime);
+    const elements = document.querySelectorAll("#content span");
 
-  setTimeout(() => {
-    clearInterval(interval);
-    span.innerHTML = targetChar;
-  }, randTime * 5);
-});
+    elements.forEach((span, index) => {
+        const targetChar = text[index];
+
+        let randTime = Math.ceil(Math.random() * 100) + 50;
+        let interval = setInterval(() => {
+            let randIndex = Math.ceil(Math.random() * (strings.length - 1));
+            span.innerHTML = strings[randIndex];
+        }, randTime);
+
+        setTimeout(() => {
+            clearInterval(interval);
+            span.innerHTML = targetChar;
+        }, randTime * 5);
+    });
+    index = (index + 1) % textList.length;
+};
